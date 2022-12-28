53ae347867454843a281f70bbab9a232e525f50e
Stefano-Vitelli <stefanovitelli9@gmail.com>
2022-12-28 13:34:54 +0100

    fetch data from firebase and POST request

--- src/App.js
+++ src/App.js
@@ -1,32 +1,85 @@
-import React from 'react';
-import './App.css';
-import MovieList from './components/MovieList';
-
+import React, { useState, useEffect, useCallback } from "react";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import AddMovie from "./components/AddMovie";
 
 function App() {
-  const dummyMovies = [{
-    id: 1,
-    title: 'Dummy movie 1',
-    openingText: 'This is a story tell by...',
-    releaseDate: '2021-05-18'
-  },
-  {
-    id: 2,
-    title: 'Dummy movie 2',
-    openingText: 'This is a story tell by...',
-    releaseDate: '2021-05-18'
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("https://react-http-744a2-default-rtdb.firebaseio.com/movies.json");
+
+      if (!response.ok) {
+        throw new Error("Something went wrong"); // the condition of the error is before parsing data.
+      }
+
+      const data = await response.json();
+
+      let loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+      
+      setMovies(loadedMovies);
+    } 
+    catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, [] );
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler])
+
+  const addMovieHandler = async (movie) => {
+   const response = await fetch('https://react-http-744a2-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
   }
-]
-  
+
+  let conditionalContent = <p> Try to fetch a good movie </p>
+  if (movies.length > 0) {
+    conditionalContent = <MovieList movies={movies} />
+  }
+
+  if (error) {
+    conditionalContent = <p> {error} </p>
+  }
+
+  if (isLoading) {
+    conditionalContent = <p> Loading Page...</p>
+  }
   return (
-  <React.Fragment>
-    <section>
-      <button> Fetch Movies</button>
-    </section>
-    <section>
-      <MovieList movies={dummyMovies} />
-    </section>
-  </React.Fragment>
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}> Fetch Movies</button>
+      </section>
+      <section>
+        {conditionalContent}
+      </section>
+    </React.Fragment>
   );
 }
 
